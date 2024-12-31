4046a4ba5c3c37d51fef8aff9a8eaa8b296b66de
swipe <swipe@MacBook-Air-SWIPE-2.local>
2024-12-31 22:57:02 +0200

    ripple images animation fixed

--- logic/button-ripple-effect.js
+++ logic/button-ripple-effect.js
@@ -1,5 +1,86 @@
+const options = document.getElementById("options");
+const hiddenInput = document.getElementById("hiddenInput");
+const nextButton = document.getElementById("nextButton");
+
+function updateNextButton(isOptionSelected) {
+  if (isOptionSelected) {
+    nextButton.style.backgroundColor = "rgb(2, 8, 45)";
+    nextButton.classList.remove("cursor-not-allowed");
+    nextButton.classList.add("cursor-pointer");
+    nextButton.disabled = false;
+  } else {
+    nextButton.style.backgroundColor = "#e0e0e0";
+    nextButton.classList.add("cursor-not-allowed");
+    nextButton.classList.remove("cursor-pointer");
+    nextButton.disabled = true;
+  }
+}
+
+function resetOptions() {
+  document.querySelectorAll(".option").forEach(option => {
+    option.classList.remove("bg-[#00c891]", "text-white");
+    option.classList.add("bg-gray-100", "text-[#535c6e]");
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedValue = getCookie(`selectedOptions-${pageId}`);
+  let isOptionSelected = false;
+
+  resetOptions();
+
+  if (savedValue) {
+    const selectedOption = Array.from(
+      document.querySelectorAll(".option")
+    ).find(option => option.dataset.value === JSON.parse(savedValue));
+
+    if (selectedOption) {
+      selectedOption.classList.add("bg-[#00c891]", "text-white");
+      selectedOption.classList.remove("bg-gray-100", "text-[#535c6e]");
+      hiddenInput.value = JSON.parse(savedValue);
+      isOptionSelected = true;
+    }
+  } else {
+    const firstOption = document.querySelector(".option");
+    if (firstOption) {
+      firstOption.classList.add("bg-[#00c891]", "text-white");
+      firstOption.classList.remove("bg-gray-100", "text-[#535c6e]");
+      hiddenInput.value = firstOption.dataset.value;
+      isOptionSelected = true;
+    }
+  }
+
+  updateNextButton(isOptionSelected);
+});
+
+options.addEventListener("click", e => {
+  if (e.target.closest(".option")) {
+    const option = e.target.closest(".option");
+
+    resetOptions();
+
+    option.classList.remove("bg-gray-100", "text-[#535c6e]");
+    option.classList.add("bg-[#00c891]", "text-white");
+
+    const selectedValue = option.dataset.value;
+
+    hiddenInput.value = selectedValue;
+
+    updateNextButton(true);
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  const selectedValue = hiddenInput.value;
+  if (selectedValue) {
+    setCookie(`selectedOptions-${pageId}`, selectedValue, 1);
+
+    window.location.href = "page-23.html";
+  }
+});
+
 function toggleActive(button, event) {
-  const buttons = document.querySelectorAll("#options button rippleBtn");
+  const buttons = document.querySelectorAll("#options button");
   buttons.forEach(btn => {
     btn.classList.remove("active-button");
     const filterSpan = btn.querySelector(".filter-span");
